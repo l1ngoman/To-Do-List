5a5a4f7f4ad1d556099d6423c2126984acb9433a
l1ngoman <athomasgarrett@gmail.com>
2018-10-25 16:18:08 -0700

    Working on sorting method in TaskList

--- taskList.rb
+++ taskList.rb
@@ -1,9 +1,11 @@
+require 'date'
 class Task
-  attr_accessor :title, :desc, :done
+  attr_accessor :title, :desc, :done, :date
   def initialize(title, desc)
     @title = title
     @desc = desc
     @done = false
+    @date = 0
   end
 
   def is_done
@@ -11,14 +13,10 @@ class Task
   end
 
   def print_task
-    p "#{@title}: #{@desc}"
-    if @done == true
-      "Status: Complete!"
-    else
-      "Status: INCOMPLETE! Fix your life!!!"
-    end
+    msg = "#{@title}: #{@desc}. Completed? #{@done}!"
+    p msg
+    msg
   end
-
 end
 
 class TaskList
@@ -35,21 +33,55 @@ class TaskList
     end
 
     def print_incomplete
+      msg_arr = []
       @incomplete.each do |el|
-        p "#{el.title}: #{el.desc}. Completed? #{el.done}"
+        msg_arr << "#{el.title}: #{el.desc}. Due: #{el.date} Completed? #{el.done}!"
       end
+      p msg_arr
+      return msg_arr
     end
 
     def print_complete
+      msg_arr = []
       @complete.each do |el|
-        p "#{el.title}: #{el.desc}. Completed? #{el.done}"
+        msg_arr << "#{el.title}: #{el.desc}. Due: #{el.date} Completed? #{el.done}!"
       end
+      p msg_arr
+      return msg_arr
     end
+    # def sort_tasks
+    #   @incomplete.sort! { |task| task.date }
+    #   i = 0
+    #   while @incomplete[i] == 0
+    #     @incomplete << @incomplete.shift
+    #     i += 1
+    #   end
+    #
+    # end
+
+end
+
+class DueDateTask < Task
+  attr_accessor :date
+  def initialize(title, desc, date) #date object
+    super(title, desc)
+    @date = Date.parse(date)
+  end
+
+  def print_task
+    msg = "#{@title}: #{@desc}. Due: #{@date} Completed? #{@done}!"
+    p msg
+    msg
+  end
 end
 
 
 tasklist1 = TaskList.new
-tasklist1.print_complete()
 task2 = Task.new("Fold Laundry", "Don't let it get wrinkly")
+testDate = DueDateTask.new("Take out trash","Don't be lazy","October 30th, 2018")
+testDate2 = DueDateTask.new("Take out trash","Don't be lazy","October 28th, 2018")
 tasklist1.add(task2)
+tasklist1.add(testDate)
+tasklist1.add(testDate2)
+tasklist1.sort_tasks
 tasklist1.print_incomplete
--- taskList_spec.rb
+++ taskList_spec.rb
@@ -1,19 +1,17 @@
-# Story: As a developer, I can create a Task.
+# Story: As a developer, I can create a DueDateTask, which is-a Task that has-a due date.
+# Hint: Use the built-in Ruby Date class
 #
-# Story: As a developer, I can give a Task a title and retrieve it.
+# Story: As a developer, I can print an item with a due date with labels and values.
+# Hint: When implementing to_s, use super to call to_s on the super class.
 #
-# Story: As a developer, I can give a Task a description and retrieve it.
+# Story: As a developer, I can add items with due dates to my TaskList.
+# Hint: Consider keeping items with due dates separate from the other items.
 #
-# Story: As a developer, I can mark a Task done.
+# Story: As a developer with a TaskList, I can list all the not completed items that are due today.
 #
-# Story: As a developer, when I print a Task that is done, its status is shown.
+# Story: As a developer with a TaskList, I can list all the not completed items in order of due date.
 #
-# Story: As a developer, I can add all of my Tasks to a TaskList.
-# Hint: A TaskList has-many Tasks
-#
-# Story: As a developer with a TaskList, I can get the completed items.
-#
-# Story: As a developer with a TaskList, I can get the incomplete items.
+# Story: As a developer with a TaskList with and without due dates, I can list all the not completed items in order of due date, and then the items without due dates.
 
 
 require 'rspec'
@@ -21,13 +19,13 @@ require_relative 'taskList.rb'
 
 describe Task do
   before do
-    @task1 = Task.new("myTask", 'desc')
+    @task1 = Task.new("title", 'desc')
   end
   it "has to be real" do
     expect{Task.new("title","desc")}.to_not raise_error
   end
   it "can set its attributes" do
-    expect(@task1.title).to eq "myTask"
+    expect(@task1.title).to eq "title"
     expect(@task1.desc).to eq 'desc'
     expect(@task1.done).to eq false
   end
@@ -36,9 +34,9 @@ describe Task do
     expect(@task1.done).to eq true
   end
   it "can print itself and show status" do
-    expect(@task1.print_task).to eq "Status: INCOMPLETE! Fix your life!!!"
+    expect(@task1.print_task).to eq "title: desc. Completed? false!"
     @task1.is_done
-    expect(@task1.print_task).to eq "Status: Complete!"
+    expect(@task1.print_task).to eq "title: desc. Completed? true!"
   end
 end
 
@@ -48,6 +46,8 @@ describe TaskList do
     @task1 = Task.new('title', 'desc')
     @task2 = Task.new('title', 'desc')
     @task3 = Task.new('title', 'desc')
+    @testDate1 = DueDateTask.new("Take out trash","Don't be lazy","October 30th, 2018")
+    @testDate2 = DueDateTask.new("Take out trash","Don't be lazy","October 28th, 2018")
   end
   it "has to be real" do
     expect{TaskList.new}.to_not raise_error
@@ -61,5 +61,28 @@ describe TaskList do
     @TaskList1.add(@task2)
     @TaskList1.add(@task3)
     expect(@TaskList1.incomplete).to eq [@task1, @task2, @task3]
+  end
+  it "can print incomplete array" do
+    @TaskList1.add(@task2)
+    expect(@TaskList1.print_incomplete).to eq ["title: desc. Due: 0 Completed? false!"]
+  end
+  #TODO test complete array once we make a method to do that
+  it "Sorting incomplete tasks, prioritizes Due Dates" do
+    @TaskList1.add(@task1)
+    @TaskList1.add(@task2)
+    @TaskList1.add(@testDate1)
+    @TaskList1.sort_tasks
+    expect(@TaskList1.print_incomplete).to eq ["Take out trash: Don't be lazy. Due: 2018-10-30 Completed? false!", "title: desc. Due: 0 Completed? false!","title: desc. Due: 0 Completed? false!"]
+  end
+end
+
+describe DueDateTask do
+  before do
+    @DueDateTask1 = DueDateTask.new("title", "desc", "October 30th, 2018")
+  end
+  it "can print itself and show status" do
+    expect(@DueDateTask1.print_task).to eq "title: desc. Due: 2018-10-30 Completed? false!"
+    @DueDateTask1.is_done
+    expect(@DueDateTask1.print_task).to eq "title: desc. Due: 2018-10-30 Completed? true!"
   end
 end
